48d331d69746ec9237175496fc6aeff161ef393f
Marcus Perryman <marcpems@msn.com>
2019-06-25 17:07:03 +0100

    Initial commit

---
+++ ReadApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+namespace ReadApp
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.IO.Ports;
+    using Windows.UI.Popups;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<string> ReceivedData { get; } = new ObservableCollection<string>();
+        private SerialPort port;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                ReceivedData.Add(port.ReadExisting());
+            });
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                port = new SerialPort("COM3");
+                port.DataReceived += Port_DataReceived;
+                port.Open();
+                ReceivedData.Add("COM3 port is now open and listening...");
+            }
+            catch (Exception ex)
+            {
+                await new MessageDialog(ex.ToString()).ShowAsync();
+            }
+        }
+    }
+}
---
+++ SerialPortApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+namespace SerialPortApp
+{
+    using System;
+    using System.IO.Ports;
+    using Windows.UI.Popups;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void SeralButton2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var p = new SerialPort("COM4");
+                p.Open();
+                p.WriteLine(DateTimeOffset.UtcNow.ToString());
+                p.Close();
+            }
+            catch (Exception ex)
+            {
+                await new MessageDialog(ex.ToString() + "\r\n\r\nDid you start the ReadApp and open the COM port?", "Error").ShowAsync();
+            }
+        }
+    }
+}
